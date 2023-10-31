ea26865090521f4374d041c1dfd4eca8e7df8e38
Timmy0618 <timmy85618@gmail.com>
2023-10-31 16:04:49 +0800

    優化fetso物件、新增讀取當前壓力值

--- modules/festo.py
+++ modules/festo.py
@@ -26,16 +26,33 @@ class festo:
                   (", ".join(hex(b) for b in self.data)),
                   (", ".join(hex(b) for b in self.crc16))))
 
+    def readSetPressure(self, id):
+        inputData = [id, 3, 0, 0, 0, 1]
+        crc16 = self.__crc16_maxim(inputData)
+        inputData += crc16
+        bytes_data = bytearray(inputData)
+        self.ser.write(bytes_data)
+        self.__print_bytes_array(bytes_data)
+        time.sleep(0.5)  # wait 0.5s
+        response = self.ser.read(7)
+        self.__print_bytes_array(response, flag='read')
+        print("CRC checK: ", self.__checkCRC(response[:-2], response[-2:]))
+        self.__setValue(response)
+        result = round(int.from_bytes(self.data, "big")/100-100, 2)
+
+        return result
+
     # Read the vacuum pressure value of pressure sensor feedback
     def readVacuumPressure(self, id):
         inputData = [id, 4, 0, 1, 0, 1]
         crc16 = self.__crc16_maxim(inputData)
         inputData += crc16
-        self.ser.write(inputData)
-        print("write 8 byte data: ", (", ".join(hex(b) for b in inputData)))
+        bytes_data = bytearray(inputData)
+        self.ser.write(bytes_data)
+        self.__print_bytes_array(bytes_data)
         time.sleep(0.5)  # wait 0.5s
         response = self.ser.read(7)
-        print("read 7 byte data:", (", ".join(hex(b) for b in response)))
+        self.__print_bytes_array(response, flag='read')
         print("CRC checK: ", self.__checkCRC(response[:-2], response[-2:]))
         self.__setValue(response)
         result = round(int.from_bytes(self.data, "big")/100-100, 2)
@@ -47,11 +64,12 @@ class festo:
         inputData = [id, 4, 0, 2, 0, 1]
         crc16 = self.__crc16_maxim(inputData)
         inputData += crc16
-        self.ser.write(inputData)
-        print("write 8 byte data: ", (", ".join(hex(b) for b in inputData)))
+        bytes_data = bytearray(inputData)
+        self.ser.write(bytes_data)
+        self.__print_bytes_array(bytes_data)
         time.sleep(0.5)  # wait 0.5s
         response = self.ser.read(7)
-        print("read 7 byte data:", (", ".join(hex(b) for b in response)))
+        self.__print_bytes_array(response, flag='read')
         checkResult = self.__checkCRC(response[:-2], response[-2:])
         print("CRC checK: ", checkResult)
         self.__setValue(response)
@@ -66,11 +84,12 @@ class festo:
         inputData = [id, 3, 0, 3, 0, 4]
         crc16 = self.__crc16_maxim(inputData)
         inputData += crc16
-        self.ser.write(inputData)
-        print("write 8 byte data: ", (", ".join(hex(b) for b in inputData)))
+        bytes_data = bytearray(inputData)
+        self.ser.write(bytes_data)
+        self.__print_bytes_array(bytes_data)
         time.sleep(0.5)  # wait 0.5s
         response = self.ser.read(13)
-        print("read 7 byte data:", (", ".join(hex(b) for b in response)))
+        self.__print_bytes_array(response, flag='read')
         print("CRC checK: ", self.__checkCRC(response[:-2], response[-2:]))
         self.__setValue(response)
 
@@ -85,34 +104,36 @@ class festo:
 
     # Write a target pressure value
     def writePressure(self, id, pressure):
-        pressure = (pressure+100)*100
+        pressure = int((pressure + 100) * 100)
         inputData = [id, 6, 0, 0]
         pressureDec = self.__decToByteAry(pressure)
         if (len(pressureDec) == 1):
             pressureDec = [0]+pressureDec
-        crc16 = self.__crc16_maxim(inputData+pressureDec)
-        inputData = inputData+pressureDec+crc16
-
-        self.ser.write(inputData)
-        print("write 8 byte data:", (", ".join(hex(b) for b in inputData)))
-        time.sleep(0.5)  # wait 0.5s
+        crc16 = self.__crc16_maxim(inputData + pressureDec)
+        inputData += (pressureDec+crc16)
+        # Create a bytes object from the integers
+        byte_data = bytearray(inputData)
+        self.ser.write(byte_data)
+        self.__print_bytes_array(byte_data)
+        time.sleep(1)
         response = self.ser.read(8)
-        print("read 8 byte data:", (", ".join(hex(b) for b in response)))
+        self.__print_bytes_array(response, flag='read')
         checkResult = self.__checkCRC(response[:-2], response[-2:])
-        print("CRC checK: ", checkResult)
+        print("CRC check: ", checkResult)
         self.__setValue1(response)
 
         return checkResult
 
     # write Kp of proportional coefficients in PID parameters
+
     def writeKp(self, id, kp):
         inputData = [id, 6, 0, 3]
         kpDec = self.__decToByteAry(kp)
         crc16 = self.__crc16_maxim(inputData+kpDec)
         inputData = inputData+kpDec+crc16
-
-        self.ser.write(inputData)
-        print("write 17 byte data:", (", ".join(hex(b) for b in inputData)))
+        byte_data = bytearray(inputData)
+        self.ser.write(byte_data)
+        self.__print_bytes_array(byte_data)
         time.sleep(0.5)  # wait 0.5s
         response = self.ser.read(8)
         print("read 8 byte data:", (", ".join(hex(b) for b in response)))
@@ -128,12 +149,12 @@ class festo:
         stepDec = self.__decToByteAry(step)
         crc16 = self.__crc16_maxim(inputData+kpDec+kiDec+kdDec+stepDec)
         inputData = inputData+kpDec+kiDec+kdDec+stepDec+crc16
-
-        self.ser.write(inputData)
-        print("write 17 byte data:", (", ".join(hex(b) for b in inputData)))
+        byte_data = bytearray(inputData)
+        self.ser.write(byte_data)
+        self.__print_bytes_array(byte_data)
         time.sleep(0.5)  # wait 0.5s
         response = self.ser.read(8)
-        print("read 8 byte data:", (", ".join(hex(b) for b in response)))
+        self.__print_bytes_array(response, flag='read')
         print("CRC checK: ", self.__checkCRC(response[:-2], response[-2:]))
         self.__setValue(response)
 
@@ -156,6 +177,10 @@ class festo:
         return [crc[1], crc[0]]
 
     def __checkCRC(self, inputData, crcRes):
+        # Check if the lengths of inputData and crcRes are as expected
+        if len(inputData) < 2 or len(crcRes) < 2:
+            return False
+
         flag = True
         computedCRC = self.__crc16_maxim(inputData)
         for i in range(2):
@@ -174,26 +199,47 @@ class festo:
 
     # for write
     def __setValue1(self, response):
-        self.slaveId = [response[0]]
-        self.funcCode = [response[1]]
-        self.bytesCount = response[2:3]
-        self.data = response[4:6]
-        self.crc16 = response[6:]
+        # Check if the length of response is as expected (at least 7 elements)
+        if len(response) >= 7:
+            self.slaveId = [response[0]]
+            self.funcCode = [response[1]]
+            self.bytesCount = response[2:3]
+            self.data = response[4:6]
+            self.crc16 = response[6:]
+        else:
+            # Handle the case when the response doesn't have enough elements
+            # You can add appropriate error handling here
+            print("Error: Response does not have enough elements")
+
+    def __print_bytes_array(self, byte_array, flag='write'):
+        print(f"{flag} {len(byte_array)} bytes:")
+        for i in range(0, len(byte_array), 2):
+            chunk = byte_array[i:i+2]
+            print(chunk.hex(), end=' ')
+        print("\n")
 
 
 if __name__ == "__main__":
     print("Start")
-    comPort = 'COM8'
+    comPort = '/dev/cu.usbserial-140'
     festoObj = festo(comPort)
     while (True):
+        id = int(input("請輸入機器id:"))
         inputText = input("請輸入指令:")
-        print(inputText)
         if (inputText == 'chamber'):
-            chamberPressure = festoObj.readChamberPressure(1)
+            chamberPressure = festoObj.readChamberPressure(id)
             print('chamberPressure: %s' % (chamberPressure))
         if (inputText == 'vacuum'):
-            vacuumPressure = festoObj.readVacuumPressure(1)
+            vacuumPressure = festoObj.readVacuumPressure(id)
             print('vacuumPressure: %s' % (vacuumPressure))
         if (inputText == 'set'):
             pressure = input("請輸入壓力值:")
-            vacuumPressure = festoObj.writePressure(1, int(pressure))
+            vacuumPressure = festoObj.writePressure(id, float(pressure))
+        if (inputText == 'readmulti'):
+            result = festoObj.readMulti(id)
+            print(result)
+        if (inputText == 'writemulti'):
+            festoObj.writeMulti(id, 1500, 500, 800, 600)
+        if (inputText == 'readsetpressure'):
+            result = festoObj.readSetPressure(id)
+            print(result)
